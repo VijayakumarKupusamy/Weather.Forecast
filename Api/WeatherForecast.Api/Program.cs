50b06a23c95bfc2790abf950d1ed028c64094338
Vijayakumar K <vijayakumarkupusamy@outlook.com>
2023-04-07 13:56:04 +0530

    #Extension Method

--- Api/WeatherForecast.Api/Program.cs
+++ Api/WeatherForecast.Api/Program.cs
@@ -1,3 +1,5 @@
+using WeatherForecast.Api;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,9 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<WeatherForecast.Api.ITemperature, WeatherForecast.Api.Temperature>();
-builder.Services.AddSingleton<WeatherForecast.Api.IRainFall, WeatherForecast.Api.RainFall>();
-builder.Services.AddSingleton<WeatherForecast.Api.IWeatherForecast, WeatherForecast.Api.WeatherForecast>();
+builder.Services.AddWeatherForeCast();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
